b6692044c02516e28b4878a69b9e0eae414b0397
cao.zhewen <cao.zhewen@ustcinfo.com>
2024-07-18 11:46:24 +0800

    使用透明代理

--- api/index.go
+++ api/index.go
@@ -1,13 +1,18 @@
 package api
 
 import (
+	"io"
 	"log"
 	"net/http"
-	"net/http/httputil"
 	url2 "net/url"
 	"os"
+	"strings"
 	"time"
 )
+
+func GetOrigin() string {
+	return os.Getenv("Origin")
+}
 
 func GetProxyUrl() *url2.URL {
 	parse, err := url2.Parse(os.Getenv("PROXY_URL"))
@@ -24,5 +29,25 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		log.Printf("method: %s path:%s remote:%s spent:%v", r.Method, r.RequestURI, r.RemoteAddr, time.Since(start))
 	}()
-	httputil.NewSingleHostReverseProxy(GetProxyUrl()).ServeHTTP(w, r)
+	request, err := http.NewRequest(r.Method, "", r.Body)
+	if err != nil {
+		panic(err)
+	}
+	for k, v := range r.Header {
+		request.Header[k] = v
+	}
+	request.URL = GetProxyUrl().ResolveReference(r.URL)
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	for k, v := range resp.Header {
+		w.Header().Set(k, strings.Join(v, "; "))
+	}
+	w.WriteHeader(resp.StatusCode)
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
 }

7687fc4d0e9cf4343526dc60fc9883f30c92a3a3
CZW <80454999+CZwwww@users.noreply.github.com>
2024-10-12 11:47:47 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -29,6 +29,10 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		log.Printf("method: %s path:%s remote:%s spent:%v", r.Method, r.RequestURI, r.RemoteAddr, time.Since(start))
 	}()
+	if isWebSock(r) {
+		handleWebSocket(w, r)
+		return
+	}
 	request, err := http.NewRequest(r.Method, "", r.Body)
 	if err != nil {
 		panic(err)
@@ -48,6 +52,47 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(resp.StatusCode)
 	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func isWebSock(r *http.Request) bool {
+	if r.Header.Get("Connection") == "Upgrade" {
+		return true
+	}
+	return false
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	host := GetProxyUrl().Host
+	controller := http.NewResponseController(w)
+	request, err2 := httputil.DumpRequest(r, false)
+	if err2 != nil {
+		panic(err2)
+	}
+	hijack, _, err := controller.Hijack()
+	if err != nil {
+		panic(err)
+	}
+	defer hijack.Close()
+	dial, err := net.Dial("tcp", host)
+	if err != nil {
+		panic(err)
+	}
+	defer dial.Close()
+	log.Println("\n" + string(request))
+	_, err2 = dial.Write(request)
+	if err2 != nil {
+		panic(err2)
+	}
+	go func() {
+		_, err2 := io.Copy(hijack, dial)
+		if err2 != nil {
+			log.Println(err2)
+		}
+	}()
+	_, err = io.Copy(dial, hijack)
 	if err != nil {
 		log.Println(err)
 	}
